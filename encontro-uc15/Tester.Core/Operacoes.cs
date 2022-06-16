dafbfbaa08315e6702e75778a082587b072bc92c
JPN20 <75049978+JPN20@users.noreply.github.com>
2022-06-16 18:16:59 -0300

    Add files via upload

---
+++ encontro-uc15/Tester.Core/Operacoes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Tester.Core
+{
+    public static class Operacoes
+    {
+        public static double Somar(double primeiroNumero, double segundoNumero)
+        {
+            return (primeiroNumero + segundoNumero);
+        }
+
+        public static double Multiplicar(double primeiroNumero, double segundoNumero)
+        {
+            return (primeiroNumero * segundoNumero); 
+        }
+    }
+}
